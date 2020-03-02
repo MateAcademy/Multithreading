8004d604d98841f708f830c78dc0f14b5977838f
Sergey Klunniy <s.klunniy@gmail.com>
2020-03-02 22:41:08 +0200

    delete sout in main

--- src/Test.java
+++ src/Test.java
@@ -6,7 +6,6 @@ public class Test {
         MyThread myThread = new MyThread();
         myThread.start();
 
-        System.out.println("Hello from main thread");
         for (int i=0; i<100; i++) {
             System.out.println("Hello from MAIN thread: " + i);
         }
