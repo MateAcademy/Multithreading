8a26fb23cf6b9c7a5d4e4d060c034d48cc29d71f
Sergey Klunniy <s.klunniy@gmail.com>
2020-03-02 23:26:05 +0200

    add Thread.sleep(50) in class Runner and changed sout on system.err.println()

--- src/Runner.java
+++ src/Runner.java
@@ -6,7 +6,13 @@ public class Runner implements Runnable {
     @Override
     public void run() {
         for (int i = 0; i < 100; i++) {
-            System.out.println("Hello from class Runner: " + i);
+            try {
+                Thread.sleep(50);
+            } catch (InterruptedException e) {
+                new DataProcessingException("" +
+                        "method run throws exception in class Runner" + e);
+            }
+            System.err.println("Hello from class Runner: " + i);
         }
     }
 }
